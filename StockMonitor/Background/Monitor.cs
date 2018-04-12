003ce05c6de096a1f521d955c5b529a502814e62
kevin-jing <jingqf@hotmail.com>
2018-04-12 15:16:02 +0800

    read stock info from sina

--- StockMonitor/Background/Monitor.cs
+++ StockMonitor/Background/Monitor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
@@ -12,17 +13,26 @@ namespace StockMonitor.Background
     {
         private readonly IHubContext<PushHub> _hubContext;
         private readonly TransactionContext _transactionContext;
+        private HttpClient _httpClient;
         public Monitor(IHubContext<PushHub> hubContext, TransactionContext transactionContext)
         {
             _hubContext = hubContext;
             _transactionContext = transactionContext;
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("http://hq.sinajs.cn/");
         }
         protected override async Task ExecuteAsync(CancellationToken cancellationToken)
         {
             while (!cancellationToken.IsCancellationRequested)
             {
                 var transaction = await _transactionContext.Transactions.SingleOrDefaultAsync(m => m.Id == 1);
-                await _hubContext.Clients.All.SendAsync("BroadcastMessage", "Transaction: ", $"{transaction.StockId}");
+
+                string requestUri = $"list={transaction.StockId}";
+
+                HttpResponseMessage resp = await _httpClient.GetAsync(requestUri);
+                var content = await resp.Content.ReadAsStringAsync();
+
+                await _hubContext.Clients.All.SendAsync("BroadcastMessage", "Transaction: ", $"{content}");
 
                 await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
             }
