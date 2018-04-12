0c373065284df21e687924c3d3a004da061114b1
kevin-jing <jingqf@hotmail.com>
2018-04-12 16:24:15 +0800

    get name and price from sina

--- StockMonitor/Background/Monitor.cs
+++ StockMonitor/Background/Monitor.cs
@@ -27,13 +27,22 @@ namespace StockMonitor.Background
             {
                 var transaction = await _transactionContext.Transactions.SingleOrDefaultAsync(m => m.Id == 1);
 
-                string requestUri = $"list={transaction.StockId}";
+                //string requestUri = $"list={transaction.StockId}";
+                string requestUri = $"list=s_{transaction.StockId}";
 
                 HttpResponseMessage resp = await _httpClient.GetAsync(requestUri);
                 var content = await resp.Content.ReadAsStringAsync();
+                // var hq_str_s_sh603577="汇金通,13.650,-0.130,-0.94,47190,6420";
+                // 指数名称，当前点数，当前价格，涨跌率，成交量（手），成交额（万元）
 
-                await _hubContext.Clients.All.SendAsync("BroadcastMessage", "Transaction: ", $"{content}");
+                var infos = content.Substring(23).Split(','); // 汇金通,13.650,-0.130,-0.94,47190,6420";
+                var name = infos[0];
+                var currentPrice = float.Parse(infos[1]);
 
+                await _hubContext.Clients.All.SendAsync("BroadcastMessage", "Transaction: ", $"{name},{currentPrice}");
+
+
+                //await _hubContext.Clients.All.SendAsync("BroadcastMessage", "Transaction: ", "123");
                 await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
             }
         }
